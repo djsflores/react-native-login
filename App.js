ac19d29952b09f190d05ca9aebda433cd5ffd3a4
David Jesús Sebastián Flores <davidjesussebastianflores@iMac-de-David.local>
2023-04-17 21:32:45 -0300

    firebase

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { GoogleAuthProvider, getAuth, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import app from './firebase';
+import { useEffect } from 'react';
 
 export default function App() {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  
+  const onAuth = () =>{
+    signInWithRedirect(auth, provider);
+  }
+
+  const getSignInData = () => {
+    getRedirectResult(auth)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result?.user;
+        console.log(token, user)
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getSignInData();
+  }, [])
+  
+
+  console.log(auth);
+  console.log(provider);
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableHighlight onPress={onAuth}>
+        <View style={styles.button}>
+          <Text>Sign-in with Google</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +47,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: 'gray',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
   },
 });
---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8OiboTujUh7SXItf7GjFG0CXE3hHmnUc",
+  authDomain: "react-native-firebase-f4002.firebaseapp.com",
+  projectId: "react-native-firebase-f4002",
+  storageBucket: "react-native-firebase-f4002.appspot.com",
+  messagingSenderId: "478394210339",
+  appId: "1:478394210339:web:29076e00f4e017442b7996"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
